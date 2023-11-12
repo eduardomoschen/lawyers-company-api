9ccf0249821d797c9ee1207c4e6a663be7e786d9
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-12 18:09:26 -0300

    correção de escrita

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'test_framework_simplejwt',
+    'rest_framework_simplejwt',
 
     'lawyers',
     'company'
