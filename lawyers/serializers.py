c828748b83006ab9a541f6245bcba807618bec9d
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-15 00:47:19 -0300

    docs: documentação do serializer

--- lawyers/serializers.py
+++ lawyers/serializers.py
@@ -4,8 +4,24 @@ from company.serializers import CompanySerializer
 
 
 class LawyerSerializer(ModelSerializer):
+    """
+    Serializer para o modelo Lawyer.
+
+    Atributos:
+        company: usado para listar as empresas que o advogado trabalha.
+
+    Métodos:
+        Não há métodos neste serializer.
+
+    Campos:
+        name: Nome do advogado.
+        username: Username mostrado no perfil do advogado.
+        bio: Descrição da biografia do advogado
+        company: Empresa na qual o advogado está associado.
+    """
+
     company = CompanySerializer()
 
     class Meta:
         model = Lawyer
-        fields = ('username', 'bio', 'company')
+        fields = ('name', 'username', 'bio', 'company')
