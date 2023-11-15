6809eaecbd2ca591d8a30ad658a26954fb9c0309
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-15 00:47:00 -0300

    docs: documentação das permissões da autenticação

--- lawyers/permissions.py
+++ lawyers/permissions.py
@@ -2,7 +2,32 @@ from rest_framework.permissions import BasePermission
 
 
 class IsOwnerOrReadOnly(BasePermission):
+    """
+    Classe de permissão personalizada para verificar se o usuário autenticado
+    é o proprietário do perfil.
+
+    Atributos:
+        Não há atributos nesta classe.
+
+    Métodos:
+        has_object_permission: Verifica se o usuário autenticado é o
+        proprietário do perfil.
+    """
+
     def has_object_permission(self, request, view, obj):
+        """
+        Verifica se o usuário autenticado é o proprietário do objeto fornecido.
+
+        Parâmetros:
+            request: O objeto da solicitação HTTP.
+            view: A view que está sendo acessada.
+            obj: O objeto específico que está sendo acessado.
+
+        Retorna:
+            bool: True se o usuário for o proprietário do perfil, False caso
+            contrário.
+        """
+
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
 
