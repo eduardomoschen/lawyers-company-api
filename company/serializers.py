c7a184c6e5d310069785a8603d3c74d04b042368
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-16 09:53:46 -0300

    docs: documentação do serializer da app Company

--- company/serializers.py
+++ company/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from company.models import Company
-from lawyers.models import Lawyer
 
 
 class CompanySerializer(ModelSerializer):
+    """
+    Serializer para o modelo Company.
+
+    Atributos:
+        employee_count: Campo calculado que representa a quantidade de
+        advogados associados à empresa.
+
+    Métodos:
+        get_employee_count: Método que retorna a quantidade de advogados
+        associados à empresa.
+
+    Campos:
+        Todos os campos do modelo Company
+    """
+
     employee_count = SerializerMethodField(read_only=True)
 
     class Meta:
@@ -11,5 +25,15 @@ class CompanySerializer(ModelSerializer):
         fields = '__all__'
 
     def get_employee_count(self, object):
+        """
+        Obtém o número de advogados associados à empresa.
+
+        Atributos:
+            object: Instância do objeto company.
+
+        Retorna:
+            count: O número de advogados associados à empresa.
+        """
+
         count = object.lawyer_set.count()
         return count
