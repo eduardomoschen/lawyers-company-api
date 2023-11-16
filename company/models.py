18d22fc65617a81300cdeae6ecb349492ec58360
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-16 09:53:28 -0300

    docs: documentação da models da app Company

--- company/models.py
+++ company/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 
 class Company(models.Model):
+    """
+    Representação da empresa de advogados.
+
+    Atributos:
+        name: O nome da empresa.
+        bio: Uma descrição da empresa.
+
+    Métodos:
+        __str__: Retorna uma representação em string do nome da empresa.
+    """
+
     name = models.CharField(max_length=200)
     bio = models.CharField(max_length=250, blank=True, null=True)
 
